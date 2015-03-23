8aa519c56615888ca354c89c65820b6ba9dcda4e
Alejandro Dominguez Borroto <adborroto90@gmail.com>
2015-03-23 16:19:16 -0400

    Highlight results

--- Library.Web/Elasticsearch/SearchService.cs
+++ Library.Web/Elasticsearch/SearchService.cs
@@ -8,11 +8,29 @@ namespace Library.Web.Controllers
 {
     public class SearchService
     {
+        public string PreHighlightTag
+        {
+            get { return @"<strong>"; }
+        }
+
+        public string PostHighlightTag
+        {
+            get { return @"</strong>"; }
+        }
+
+
         public IEnumerable<IHit<Book>> Find(string query, int page=0,int pageSize=10)
         {
            var result=  Elasticsearch.Elasticsearch.Client.Search<Book>(s => s
                 .From(page*pageSize)
                 .Size(pageSize)
+                .Highlight(h => h
+                    .PreTags(PreHighlightTag)
+                    .PostTags(PostHighlightTag)
+                    .OnFields(f=>f
+                        .OnField(b => b.Foreword)
+                        .OnField(b=>b.Title)
+                        ))
                 .Query(q => q.QueryString(qs => qs.Query(query))));
 
             return result.Hits;
